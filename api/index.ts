eeb41b9c26f4f7d67c7a78e038c8f42cc62df362
asjidimtiaz <asjidimtiaz11@email.com>
2026-04-08 18:49:09 +0500

    auto-commit updates

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import app from '../artifacts/api-server/src/app';
 
-// Export the Express app to be used as a Serverless Function by Vercel
+// Export the Express app as a Vercel Serverless Function handler.
+// Vercel intercepts requests and passes them to this Express app instance.
 export default app;
